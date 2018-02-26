3eea3fa1d959f90d8217b458fdc090d0920cf642
Lupe <lupe@acoorg.com>
2018-02-26 10:54:45 -0800

    Build view object with feed and error handling

--- dist/main.js
+++ dist/main.js
@@ -7,8 +7,8 @@ var rssModel = {
     var articles = [];
     $.when(rssModel.ajaxCall(CORS_HEADER + "https://www.osha.gov/news/newsreleases.xml"), rssModel.ajaxCall(CORS_HEADER + "http://sunnewsreport.com/feed/")).done(function (firstCall, secondCall) {
       for (var i = 0; i < MAX_ARTICLES; i++) {
-        articles.push(firstCall[0].getElementsByTagName('item')[i]);
-        articles.push(secondCall[0].getElementsByTagName('item')[i]);
+        articles.push(firstCall[0].getElementsByTagName("item")[i]);
+        articles.push(secondCall[0].getElementsByTagName("item")[i]);
       }
       rssController.init(articles);
     }).fail(function () {
@@ -34,9 +34,25 @@ var rssController = {
         url: articles[index].getElementsByTagName("link")[0].childNodes[0].nodeValue
       };
     });
-    console.log(articles);
+    rssView.showFeed(articles);
   },
-  handleError: function handleError() {}
+  handleError: function handleError() {
+    rssView.handleError();
+  }
+};
+
+var rssView = {
+  showFeed: function showFeed(articles) {
+    $(".rssLoader").hide();
+    articles.forEach(function (article) {
+      $("#rssNewsFeedTable").append("<tr><td><a href='" + article.url + "' target='_blank'>" + article.name + "</a></td><td><a href='" + article.url + "' target='_blank'><i class='fa fa-external-link'></i></a></tr>");
+    });
+  },
+
+  handleError: function handleError() {
+    $(".rssLoader").hide();
+    $("#rssNewsFeedTable").append("<p>Sorry, something went wrong! Please contact a site administrator.</p>");
+  }
 };
 
 $(document).ready(rssModel.init);
--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ var rssModel = {
       rssModel.ajaxCall(CORS_HEADER + "http://sunnewsreport.com/feed/")
     ).done(function(firstCall, secondCall) {
       for (let i = 0; i < MAX_ARTICLES; i ++) {
-        articles.push(firstCall[0].getElementsByTagName('item')[i]);
-        articles.push(secondCall[0].getElementsByTagName('item')[i]);
+        articles.push(firstCall[0].getElementsByTagName("item")[i]);
+        articles.push(secondCall[0].getElementsByTagName("item")[i]);
       }
       rssController.init(articles);
     }).fail(function() {
@@ -29,16 +29,30 @@ var rssModel = {
 var rssController = {
   init: (articles) => {
     let articleObjects = [];
-    articles.forEach((article, index) => {
+    articles.forEach( (article, index) => {
       articles[index] = {
         name: articles[index].getElementsByTagName("title")[0].childNodes[0].nodeValue,
         url: articles[index].getElementsByTagName("link")[0].childNodes[0].nodeValue
       }
     });
-    console.log(articles);
+    rssView.showFeed(articles);
   },
   handleError: () => {
+    rssView.handleError();
+  }
+}
 
+var rssView = {
+  showFeed: (articles) => {
+    $(".rssLoader").hide();
+    articles.forEach( (article) => {
+      $("#rssNewsFeedTable").append("<tr><td><a href='" + article.url + "' target='_blank'>" + article.name + "</a></td><td><a href='" + article.url + "' target='_blank'><i class='fa fa-external-link'></i></a></tr>");
+    });
+  },
+
+  handleError: () => {
+    $(".rssLoader").hide();
+    $("#rssNewsFeedTable").append("<p>Sorry, something went wrong! Please contact a site administrator.</p>");
   }
 }
 
